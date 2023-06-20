c503b739a26b8cd74c2cc113a9d4eff0f4607f8e
Picasso <urvishp13@gmail.com>
2023-06-20 16:46:10 -0400

    edited foul() method, changed parseInts to Number() and commented out defend()

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -35,8 +35,8 @@ function putOnDefense() {
     return Math.floor( Math.random() * 61 ) - 10
 }
 
-function changePossession(dribbler) {
-    newPossession(dribbler)
+function changePossession(newDribbler) {
+    newPossession(newDribbler)
 }
 
 let click = 0
@@ -49,59 +49,72 @@ function dribble() {
         const currDribble = Math.floor( Math.random() * 31 ) - 5
 
         if (currDribble < 0) { // if YOU randomly get a negative number of this dribble
-            turnover()         // you caused a turnover
+            turnover("YOU")         // YOU caused a turnover
             return             // possession over
         }
 
         you += currDribble
     }
 
+    // if ME committed a foul while YOU were dribbling
+    if (me < 0) {
+        // change possession --> give ME the ball after YOU shoots free throws
+        foul("ME")
+        changePossession("ME")
+    }
+
     // console.log("click", click)
     // console.log("me", me, "you", you)
 }
 
+function foul(committer) {
+    if (committer === "ME") {
+        youDisp.textContent = Number(youDisp.textContent) + 1
+    }
+}
+
 function shoot() {
     // if after 3 dribbles
-        // YOU > ME by 10 or more points, 3 points scored by YOU
-        // YOU > ME by 1 or more points, 2 points scored by YOU
+        // YOU > ME by 10, 3 points scored by YOU
+        // YOU > ME by 1, 2 points scored by YOU
         // If ME is negative, YOU get one point (ME committed a foul)
         // If YOU are negative, YOU committed a turnover
     if ( (you - me) >= 10 ) {
-        youDisp.textContent = parseInt(youDisp.textContent) + 3
+        youDisp.textContent = Number(youDisp.textContent) + 3
     } else if (you > me) {
-        youDisp.textContent = parseInt(youDisp.textContent) + 2
+        youDisp.textContent = Number(youDisp.textContent) + 2
     }
     // change in possession
+    changePossession("ME")
 }
 
-function foul() {
-    if (me < 0) {
-        youDisp.textContent = parseInt(youDisp.textContent) + 1
+function turnover(violator) {
+    // change of possession
+    if (violator === "YOU") {
+        changePossession("ME") 
+    } else {
+        changePossession("YOU")
     }
-}
-
-function turnover() {
-    defend() // change of possession
 }
      
-// create a defend function --> opposite of dribble function i.e. ME has the ball now and YOU are defending
-function defend() {
-    const you = Math.floor( Math.random() * 61 ) - 10
-    let me = 0
+// // create a defend function --> opposite of dribble function i.e. ME has the ball now and YOU are defending
+// function defend() {
+//     const you = Math.floor( Math.random() * 61 ) - 10
+//     let me = 0
 
-    for (let i = 0; i < 3; i++) {
-        me += Math.floor( Math.random() * 31 ) - 5
-    }
+//     for (let i = 0; i < 3; i++) {
+//         me += Math.floor( Math.random() * 31 ) - 5
+//     }
 
-    if ( (me - you) >= 10 ) { // ME scores 3 points
-        meDisp.textContent = parseInt(meDisp.textContent) + 3
-    } else if (me > you) { // ME scores 2 points
-        meDisp.textContent = parseInt(meDisp.textContent) + 2
-    } else if (you < 0) { // YOU foul
-        meDisp.textContent = parseInt(meDisp.textContent) + 1
-    } else if (me < 0) { // ME causes turnover
-        dribble()
-    } else { // me < you --> change in possession
-        dribble()
-    }
-}
+//     if ( (me - you) >= 10 ) { // ME scores 3 points
+//         meDisp.textContent = Number(meDisp.textContent) + 3
+//     } else if (me > you) { // ME scores 2 points
+//         meDisp.textContent = Number(meDisp.textContent) + 2
+//     } else if (you < 0) { // YOU foul
+//         meDisp.textContent = Number(meDisp.textContent) + 1
+//     } else if (me < 0) { // ME causes turnover
+//         dribble()
+//     } else { // me < you --> change in possession
+//         dribble()
+//     }
+// }
