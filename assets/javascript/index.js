ad48b4ea8db96242722ceae20e85387668586d19
Picasso <urvishp13@gmail.com>
2023-06-19 20:56:36 -0400

    wrote first draft of startGame()

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,17 +1,29 @@
+let you = 0;
+let me = 0;
+
+const youDisp = document.getElementById("you-points")
+const meDisp = document.getElementById("me-points")
+
+// console.log(meDisp)
+
+
 // create a start game function
-    // first possession always is given to YOU (the player)
+function startGame() {
+    /* first possession always is given to YOU (the player) */
     // give YOU a random number between -5 and 25 --> dribbling
+    you = Math.floor( Math.random() * 31 ) - 5
     // give ME (the computer) a random number between -10 and 50 --> defending
-
-    // YOU has 3 dribbles/defending opportunities to score/cause turnover: call action function
-
+    me = Math.floor( Math.random() * 61 ) - 10
+    // YOU has 3 opportunities to score: call action function
+    action()
+}
 
 // create an action function (for dribbling/defending)
     // if YOU have ball, ME gets to stop you 3 times (defend), so YOU are dribbling
         // with each dribble, add one random number to ME (between -5 and 25)
     // if after 3 times
         // YOU > ME by 10 or more points, 3 points scored by YOU
-        // YOU > ME by 3 or more points, 2 points scored by YOU
+        // YOU > ME by 1 or more points, 2 points scored by YOU
         // If ME is negative, YOU get one point (ME committed a foul)
-        // If YOU are negative, YOU commit a turnover
+        // If YOU are negative, YOU committed a turnover
      
