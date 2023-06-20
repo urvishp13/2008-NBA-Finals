f830e4f005a3fc3af2dd0d7a6f765596285c60d0
Picasso <urvishp13@gmail.com>
2023-06-20 14:53:56 -0400

    made more smaller functions to make code sensible

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -12,18 +12,31 @@ function startGame() {
        is displayed */
     youDisp.textContent = 0
     meDisp.textContent = 0
-    
-    putOnDefense(me)
-    you = givePossessionTo()
-    me = putOnDefense() // give me an initial number to start game
+
+    newPossession("YOU")
 }
 
-function givePossessionTo(player) {
+function newPossession(dribbler) {
+    if (dribbler === "YOU") {
+        you = givePossessionTo()
+        me = putOnDefense()
+    }
+    else {
+        me = givePossessionTo()
+        you = putOnDefense()
+    }
+}
+
+function givePossessionTo() {
     return 0
 }
 
 function putOnDefense() {
     return Math.floor( Math.random() * 61 ) - 10
+}
+
+function changePossession(dribbler) {
+    newPossession(dribbler)
 }
 
 let click = 0
@@ -33,12 +46,21 @@ function dribble() {
     // with each dribble (click of button), add one random number to YOU (between -5 and 25)
     click++ // function ran, so one dribble recorded
     if (click <= 3) {
-        you += Math.floor( Math.random() * 31 ) - 5
+        const currDribble = Math.floor( Math.random() * 31 ) - 5
+
+        if (currDribble < 0) { // if YOU randomly get a negative number of this dribble
+            turnover()         // you caused a turnover
+            return             // possession over
+        }
+
+        you += currDribble
     }
 
-    console.log("click", click)
+    // console.log("click", click)
     // console.log("me", me, "you", you)
+}
 
+function shoot() {
     // if after 3 dribbles
         // YOU > ME by 10 or more points, 3 points scored by YOU
         // YOU > ME by 1 or more points, 2 points scored by YOU
@@ -48,13 +70,18 @@ function dribble() {
         youDisp.textContent = parseInt(youDisp.textContent) + 3
     } else if (you > me) {
         youDisp.textContent = parseInt(youDisp.textContent) + 2
-    } else if (me < 0) {
-        youDisp.textContent = parseInt(youDisp.textContent) + 1
-    } else if (you < 0) {
-        defend()
-    } else { // you < me --> change in possession
-        defend()
     }
+    // change in possession
+}
+
+function foul() {
+    if (me < 0) {
+        youDisp.textContent = parseInt(youDisp.textContent) + 1
+    }
+}
+
+function turnover() {
+    defend() // change of possession
 }
      
 // create a defend function --> opposite of dribble function i.e. ME has the ball now and YOU are defending
