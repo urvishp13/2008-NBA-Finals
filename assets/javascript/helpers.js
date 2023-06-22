71b6683f700cdb47fe0c942c98ca3dab92dfc18e
Picasso <urvishp13@gmail.com>
2023-06-22 10:23:02 -0400

    trying to get functionality of shootFreeThrow() to work for ME player

--- assets/javascript/helpers.js
+++ assets/javascript/helpers.js
@@ -30,8 +30,8 @@ function givePossessionTo() {
 }
 
 function putOnDefense() {
-    // return Math.floor( Math.random() * 61 ) - 10
-    return -1
+    return Math.floor( Math.random() * 61 ) - 10
+    // return -1
 }
 
 function changePossession(newDribbler) {
@@ -62,20 +62,6 @@ function forcedShot(shooter) {
     }
 }
 
-// this shot can be done before 3 dribbles are completed
-function shoot(num) {
-    // get the probability of shot being successful
-    const probability = Math.random()
-
-    // if the probability > 0.5, shot is made. Increase YOUR score by num
-    if (probability > 0.5) {
-        youDisp.textContent = Number(youDisp.textContent) + num
-    }
-
-    // after shot taken, give the ball to ME
-    changePossession("ME")
-}
-
 // swap the buttons for defensive play
 function replaceOffenseBtns() {
     // remove the offense buttons from the DOM and replace it with the DEFEND button
@@ -88,6 +74,22 @@ function replaceDefenseBtns() {
     // remove the DEFEND button from the DOM and replace it with the offensive buttons
     defendBtn.style.display = "none"
     offenseBtns.style.display = "flex"
+}
+
+function beginOffensivePlay() {
+    offenseBtns.style.display = "flex"
+}
+
+function removeOffensivePlay() {
+    offenseBtns.style.display = "none"
+}
+
+function beginDefensivePlay() {
+    defendBtn.style.display = "block"
+}
+
+function removeDefensivePlay() {
+    defendBtn.style.display = "none"
 }
 
 function turnover(committer) {
@@ -103,46 +105,10 @@ function turnover(committer) {
 function foul(violator) {
     liveResults.textContent = `${violator} committed a foul.`
     if (violator === "ME") { // if ME committed foul
-        // give YOU 2 free throw attempt
+        // give YOU 2 free throw attempts
         freeThrowBtn.style.display = "block" // make the SHOOT FT button present
     } else { // YOU committed foul
         // give ME 2 free throw attempts
         shootFreeThrow("ME")
     }
-}
-
-let click = 0 // Used to count YOUR button clicks
-function shootFreeThrow(shooter) {
-    click++
-
-    // get the probability of FT being successful
-    const probability = Math.random()
-
-    // if the probability > 0.5, FT is made. Increase shooter's score by 1
-    if (probability > 0.5) {
-        if (shooter === "YOU") {
-            youDisp.textContent = Number(youDisp.textContent) + 1 // YOU are allowed to shoot free throws
-        } else {
-            meDisp.textContent = Number(meDisp.textContent) + 1 // YOU are allowed to shoot free throws
-        }
-        liveResults.textContent = `${shooter} made a free throw.`
-    } else { // if probability < 0.5, FT is missed
-        liveResults.textContent = `${shooter} missed the free throw attempt.`
-    }
-    
-    if (click => 2) { // if 2 free throws have been shot
-        click = 0 // reset SHOOT FT button click count for next time when allowed to shoot free throws
-        
-        // don't allow YOU/ME to shoot anymore free throws
-        freeThrowBtn.style.display = "none" // free throws done --> remove SHOOT FT button
-        // give the ball to foul violator after the shooter shot their free throws
-        if (shooter === "YOU") {
-            changePossession("ME")  // give ME the ball
-            defendBtn.style.display = "block" // meaning YOU are back on defense
-        } else {
-            changePossession("YOU") // give YOU the ball
-            offenseBtns.style.display = "flex" // meaning YOU have the ball again
-        }
-    } 
-
 }
--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -13,6 +13,8 @@ function startGame() {
     offenseBtns.style.display = "flex"
 }
 
+let click = 0 // used to count number of times action buttons clicked
+
 // create a dribble function 
 function dribble() {
     // YOU have ball, so ME gets to stop you 3 times (defend) aka YOU get to dribble 3 times to score
@@ -35,6 +37,8 @@ function dribble() {
 
     // if ME committed a foul while YOU were dribbling
     if (me < 0) {
+        click = 0 // reset click count to count 2 free throw attempts
+        removeOffensivePlay()
         foul("ME")
         return
     }
@@ -45,7 +49,58 @@ function dribble() {
         click = 0 // reset the click count --> preparing to count defend count
     }
 }
-     
+
+// this shot can be done before 3 dribbles are completed
+function shoot(num) {
+    // get the probability of shot being successful
+    const probability = Math.random()
+
+    // if the probability > 0.5, shot is made. Increase YOUR score by num
+    if (probability > 0.5) {
+        youDisp.textContent = Number(youDisp.textContent) + num
+    }
+
+    // after shot taken, give the ball to ME
+    changePossession("ME")
+}
+
+function shootFreeThrow(shooter) {
+    click++
+
+    // get the probability of FT being successful
+    // const probability = Math.random()
+    const probability = 0.5
+
+    // if the probability > 0.5, FT is made. Increase shooter's score by 1
+    if (probability >= 0.5) {
+        if (shooter === "YOU") {
+            youDisp.textContent = Number(youDisp.textContent) + 1 // YOU are allowed to shoot free throws
+        } else {
+            meDisp.textContent = Number(meDisp.textContent) + 1 // YOU are allowed to shoot free throws
+        }
+        liveResults.textContent = `${shooter} made a free throw.`
+    } else { // if probability < 0.5, FT is missed
+        liveResults.textContent = `${shooter} missed the free throw attempt.`
+    }
+
+    // console.log(`${shooter} shot 2 FTs`, click)
+    
+    if (click === 2) { // if 2 free throws have been shot
+        click = 0 // reset SHOOT FT button click count for next time when allowed to shoot free throws
+        
+        // don't allow YOU/ME to shoot anymore free throws
+        freeThrowBtn.style.display = "none" // free throws done --> remove SHOOT FT button
+        // give the ball to foul violator after the shooter shot their free throws
+        if (shooter === "YOU") {
+            changePossession("ME")  // give ME the ball
+            beginDefensivePlay() // YOU are back on defense
+        } else {
+            changePossession("YOU") // give YOU the ball
+            beginOffensivePlay() // YOU have the ball again
+        }
+    }
+}
+
 // create a defend function --> opposite of dribble function i.e. ME has the ball now and YOU are defending
 function defend() {
     click++
